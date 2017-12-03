5108d59a8bd9eff625f23c487620ee41eb48b4b9
stelmakhmaksim <stelmakh96@gmail.com>
2017-12-03 22:26:29 +0300

    Create annotation for random number and bean post processor for this annotation.

---
+++ main/java/quoters/InjectRandomAnnotationBeanPostProcessor.java
@@ -0,0 +1,36 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+import quoters.InjectRandomInt;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+public class InjectRandomAnnotationBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws
+            BeansException {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+            if (annotation != null) {
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int i = min + random.nextInt(max - min);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, bean, i);
+            }
+
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws
+            BeansException {
+        return bean;
+    }
+}
