d08bf2ad9f02879019ca9d2b5e7028192e154520
stelmakhmaksim <stelmakh96@gmail.com>
2017-12-04 01:29:55 +0300

    Create DeprecatedClass annotation and DeprecationHandlerBeanFactoryPostProcessor

--- main/java/quoters/Main.java
+++ main/java/quoters/Main.java
@@ -5,5 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(Quoter.class).sayQuote();
     }
 }
