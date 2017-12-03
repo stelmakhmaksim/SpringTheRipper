32df2ae9c7e69ccba437437424b1ed6455c031b3
stelmakhmaksim <stelmakh96@gmail.com>
2017-12-03 22:19:04 +0300

    Create context, interface "Quoter" and simple implementation TerminatorQuoter

---
+++ main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,19 @@
+package quoters;
+
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+    
+    private String message = "sadasdasd";
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
+    }
+}
