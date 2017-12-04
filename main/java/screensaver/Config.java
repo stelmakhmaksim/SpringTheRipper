69a83cfbd831242607a0d99ae08e7b3768758d23
stelmakhmaksim <stelmakh96@gmail.com>
2017-12-04 03:20:15 +0300

    Create color frame, config for it and custom scope registry bean factory post processor

---
+++ main/java/screensaver/Config.java
@@ -0,0 +1,39 @@
+package screensaver;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    @Bean
+    @Scope("periodical")
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame frame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(screensaver.Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(300);
+        }
+    }
+}
